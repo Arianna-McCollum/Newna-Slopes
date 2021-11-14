8a5923907567e141da5002ab579def16a40fe146
Michael Astalos <michaeldont@gmail.com>
2021-11-14 12:13:37 -0500

    models createed

---
+++ server/models/Order.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const orderSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    items: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+    }
+})
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = {};
---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+const Order = require('./Order');
+const bycryt = require('bcrypt')
+
+const userSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8
+    },
+    orders: [Order.schema]
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
