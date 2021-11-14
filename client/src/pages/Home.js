a4b5e33347fb285d97057157eade1ba63c9b041c
Ryan Dungan <rd3@knights.ucf.edu>
2021-11-14 12:50:29 -0500

    fixed error

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero'
 import OurStory from '../components/OurStory';
+import Quotes from '../components/Quotes';
 import Contact from '../components/Contact';
 
 
