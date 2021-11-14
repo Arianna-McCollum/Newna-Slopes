76d950968e1718563284a3eb0ac7d0a01800a89f
Ryan Dungan <rd3@knights.ucf.edu>
2021-11-14 12:43:35 -0500

    rendered OurStory, Quote, and Contact sections to homepage

---
+++ client/src/components/OurStory/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function OurStory() {
+  return (
+    <section>
+        Our Story....
+    </section>
+    
+  );
+}
+
+export default OurStory;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from '../components/Hero'
+import OurStory from '../components/OurStory';
+import Contact from '../components/Contact';
 
 
 function Home() {
   return (
     <div>
         < Hero />
-        <p>This is pages</p>
+        < OurStory />
+        < Quotes />
+        < Contact />
     </div>
     
   );
