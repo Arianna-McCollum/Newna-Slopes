2efcf2a326f826e9844ad3056dc2f8503615f70a
Ryan Dungan <rd3@knights.ucf.edu>
2021-11-15 15:43:25 -0500

    added story and quote section style

--- client/src/components/Quotes/index.js
+++ client/src/components/Quotes/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function Quotes() {
   return (
-    <section>
-        Quotes slider here?
+    <section className="quote">
+        <div className="quote-container">
+          <p>"Powder Snow Skiing is not fun. It's life, fully lived - life lived in a blaze of reality"
+            <br></br>
+            <span>- Dolores LaChapelle</span>
+          </p>
+        </div>
     </section>
     
   );
