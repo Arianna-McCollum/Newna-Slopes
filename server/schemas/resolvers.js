9421afe5763287635669285d72bb4a189e7cdee8
Michael Astalos <michaeldont@gmail.com>
2021-11-16 19:40:34 -0500

    pushing to switch branches

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,39 +6,40 @@ const resolvers = {
     Query: {
         users: async () => {
           return User.find()
-        },
-        products: async () => {
-          return Product.find()
-        },
-        orders: async () => {
-          return Order.find()
         }
-    },
-
-    Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-      
-            return { token, user };
-          },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const token = signToken(user);
-            return { token, user };
-          }
+        // ,
+        // products: async () => {
+        //   return Product.find()
+        // },
+        // orders: async () => {
+        //   return Order.find()
+        // }
     }
+
+    // Mutation: {
+    //     addUser: async (parent, args) => {
+    //         const user = await User.create(args);
+    //         const token = signToken(user);
+      
+    //         return { token, user };
+    //       },
+        // login: async (parent, { email, password }) => {
+        //     const user = await User.findOne({ email });
+      
+        //     if (!user) {
+        //       throw new AuthenticationError('Incorrect credentials');
+        //     }
+      
+        //     const correctPw = await user.isCorrectPassword(password);
+      
+        //     if (!correctPw) {
+        //       throw new AuthenticationError('Incorrect credentials');
+        //     }
+      
+        //     const token = signToken(user);
+        //     return { token, user };
+    //       }
+    // }
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,8 +41,7 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-        login(email: String!, password: String!): Auth
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): User
     }
 `;
 
