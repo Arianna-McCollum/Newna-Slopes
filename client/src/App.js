91559984413d1bfc60338366bcd3161ce14868b0
Ryan Dungan <rd3@knights.ucf.edu>
2021-11-17 20:58:32 -0500

    routing to signup/signin pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Footer from './components/Footer'
-import "./style.css";
+import "./css/home.css";
 import { Routes, Route} from "react-router-dom";
 
 
@@ -12,6 +12,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
+        {/* <Route path="/signup" element={<Signup />}></Route> */}
+        {/* <Route path="/login" element={<Login />}></Route> */}
         {/* <Route path="/success" element={<Success />}></Route> */}
       </Routes>
       <Footer />
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ShopNav from '../components/ShopNav';
+
+
+
+function Signup() {
+  return (
+    <div>
+        < ShopNav />
+    </div>
+    
+  );
+}
+
+export default Signup;
