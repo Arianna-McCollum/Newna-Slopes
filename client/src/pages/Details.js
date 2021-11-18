ea8f587d07695cd8b9a5f8a2860fdca5cddb5504
Electrolion <AMccollum71@gmail.com>
2021-11-17 20:56:42 -0500

    Added spinner for loading

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
-// import Cart from "../components/Cart";
+import Cart from "../components/Cart";
 import { useStoreContext } from "../utils/GlobalState";
 import {
   REMOVE_FROM_CART,
@@ -12,7 +12,7 @@ import {
 } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
-// import spinner from '../assets/spinner.gif'
+import spinner from '../assets/spinner.gif'
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
