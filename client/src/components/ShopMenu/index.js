5c9ff16b0f36affb8d6fd53828cbd0321222695f
Ian Cohen <ian_cohen90@live.com>
2021-11-19 19:28:35 -0500

    added shop functionality but not working 100%

--- client/src/components/ShopMenu/index.js
+++ client/src/components/ShopMenu/index.js
@@ -1,121 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY} from '../../utils/actions';
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
 
 function ShopMenu() {
+  const [state, dispatch] = useStoreContext();
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const {categories} = state;
 
-    // for category functionality
+  useEffect(() =>{
+    if(categoryData){
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
 
-    // const [state, dispatch] = useStoreContext();
-
-    // const { categories } = state;
-    
-    // const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-    // useEffect(() => {
-    //     if (categoryData) {
-    //       dispatch({
-    //         type: UPDATE_CATEGORIES,
-    //         categories: categoryData.categories
-    //       });
-    //       categoryData.categories.forEach(category => {
-    //         idbPromise('categories', 'put', category);
-    //       });
-    //     } else if (!loading) {
-    //       idbPromise('categories', 'get').then(categories => {
-    //         dispatch({
-    //           type: UPDATE_CATEGORIES,
-    //           categories: categories
-    //         });
-    //       });
-    //     }
-    //   }, [categoryData, loading, dispatch]);
-
-    // const handleClick = id => {
-    //     dispatch({
-    //       type: UPDATE_CURRENT_CATEGORY,
-    //       currentCategory: id
-    //     });
-    //   };
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
 
   return (
-    <div className="shop-page">
-        <div className="shop-hero">
-            <h1>The Wax Room</h1>
-        </div>
-        <h2>Get your favorite gear at the best price! Whether you are experienced or new to the slopes, browse our shop below to find a product that will suit your needs.</h2>
-        <div className="shop-container">
-            <h1>Products</h1>
-            <div className="shop-wrap">
-                <div className="shop-left">
-                    <button className="product-btn">Snowboards</button>
-                    <button className="product-btn">Skis</button>
-                    <button className="product-btn">Apparel</button>
-                    <button className="product-btn">Wax & More</button>
-                </div>
-                <div className="shop-right">
-                    <div className="product">
-                        <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
-                        <h3>Iced Coffee</h3>
-                        <h4>Price: $4</h4>
-                        <div className="btn-wrapper">
-                            <button>View</button>
-                            <button><i class="fas fa-cart-plus"></i></button>
-                        </div>
-                    </div>
-
-                    <div className="product">
-                        <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
-                        <h3>Iced Coffee</h3>
-                        <h4>Price: $4</h4>
-                        <div className="btn-wrapper">
-                            <button>View</button>
-                            <button><i class="fas fa-cart-plus"></i></button>
-                        </div>
-                    </div>
-
-                    <div className="product">
-                        <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
-                        <h3>Iced Coffee</h3>
-                        <h4>Price: $4</h4>
-                        <div className="btn-wrapper">
-                            <button>View</button>
-                            <button><i class="fas fa-cart-plus"></i></button>
-                        </div>
-                    </div>
-
-                    <div className="product">
-                        <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
-                        <h3>Iced Coffee</h3>
-                        <h4>Price: $4</h4>
-                        <div className="btn-wrapper">
-                            <button>View</button>
-                            <button><i class="fas fa-cart-plus"></i></button>
-                        </div>
-                    </div>
-
-                    <div className="product">
-                        <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
-                        <h3>Iced Coffee</h3>
-                        <h4>Price: $4</h4>
-                        <div className="btn-wrapper">
-                            <button>View</button>
-                            <button><i class="fas fa-cart-plus"></i></button>
-                        </div>
-                    </div>
-
-                    <div className="product">
-                        <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
-                        <h3>Iced Coffee</h3>
-                        <h4>Price: $4</h4>
-                        <div className="btn-wrapper">
-                            <button>View</button>
-                            <button><i class="fas fa-cart-plus"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div>
+    <h2>Choose a Category:</h2>
+    {categories.map((item) => (
+      <button
+        key={item._id}
+        onClick={() => {
+          handleClick(item._id);
+        }}
+      >
+          {item.name}
+        </button>
+        ))}
     </div>
-    
   );
 }
 
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ShopNav from '../components/ShopNav';
 import ShopMenu from '../components/ShopMenu';
-import '../css/shop.css';
+
+import ProductList from "../components/ProductList";
+import Cart from '../components/Cart';
 
 
 function Shop() {
   return (
     <div>
-        < ShopNav />
-        < ShopMenu />
+        <ShopNav />
+        <ShopMenu />
+        <ProductList/>
+        <Cart/>
     </div>
     
   );
