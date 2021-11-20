2971fb951162a3403fb63bb10321e201fc466bb5
Ryan Dungan <rd3@knights.ucf.edu>
2021-11-20 16:08:33 -0500

    ui for signup/signin pages done and mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import Signup from './pages/Signup';
+import Login from './pages/Login'
 import Footer from './components/Footer'
 import "./css/home.css";
 import { Routes, Route} from "react-router-dom";
@@ -12,8 +14,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
-        {/* <Route path="/signup" element={<Signup />}></Route> */}
-        {/* <Route path="/login" element={<Login />}></Route> */}
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         {/* <Route path="/success" element={<Success />}></Route> */}
       </Routes>
       <Footer />
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import ShopNav from '../components/ShopNav';
+import '../css/signup.css';
+import { useNavigate } from "react-router-dom";
 
 
 
 function Login() {
+  const navigate = useNavigate();
   return (
-    <div>
-        < ShopNav />
+    <div className="signup-page" id="login">
+        <div className="signup-hero">
+          <h1>Login!</h1>
+        </div>
+        <div className="signup-box">
+          <div className="bg-container">
+          <div className="form-container">
+            <form className="signup-form" id="signup-form">
+              <h2>Log in!</h2>
+              <input className="field" type="text" id="username-signup" placeholder="Username"></input>
+              <input className="field" type="email" id="email-signup" placeholder="Email"></input>
+              <input className="field" type="password" id="password-signup" placeholder="Password"></input>
+              <button className="form-btn" type="submit" >Log in</button>
+              <h3 className="">Need an account? </h3>
+              <button onClick={()=> navigate("/signup")} className="login-btn"> Register </button>
+              <h4>Go Back</h4>
+                <div className="btn-wrapper2">
+                  <button onClick={()=> navigate("/")}>Home</button>
+                  <button onClick={()=> navigate("/shop")}>Shop</button>
+                </div>
+            </form>
+            </div>
+          </div>
+        </div>
     </div>
     
   );
