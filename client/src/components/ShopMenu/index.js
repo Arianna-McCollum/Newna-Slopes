9165973a16db494c647d5cc4401e0624517a62f4
Michael Astalos <michaeldont@gmail.com>
2021-11-21 13:38:59 -0500

    works

--- client/src/components/ShopMenu/index.js
+++ client/src/components/ShopMenu/index.js
@@ -32,16 +32,46 @@ function ShopMenu() {
             });
           });
         }
-      }, [categoryData, loading, dispatch]);
+    }, [categoryData, loading, dispatch]);
 
     const handleClick = id => {
         dispatch({
           type: UPDATE_CURRENT_CATEGORY,
           currentCategory: id
         });
-      };
+    }; 
 
-  return (
+    const { currentCategory } = state;
+
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+    useEffect(() => {
+        if(data) {
+          dispatch({
+               type: UPDATE_PRODUCTS,
+              products: data.products
+            });
+            data.products.forEach((product) => {
+              idbPromise('products', 'put', product);
+            });
+        } else if (!loading) {
+          idbPromise('products', 'get').then((products) => {
+            dispatch({
+              type: UPDATE_PRODUCTS,
+             products: products
+           });
+          });
+        }
+    }, [data, loading, dispatch]);
+
+    function filterProducts() {
+        if (!currentCategory) {
+          return state.products;
+        }
+    
+        return state.products.filter(product => product.category._id === currentCategory);
+    }
+
+    return (
     <div className="shop-page">
         <div className="shop-hero">
             <h1>The Wax Room</h1>
@@ -51,11 +81,18 @@ function ShopMenu() {
             <h1>Products</h1>
             <div className="shop-wrap">
                 <div className="shop-left">
-                {categories.map(item => (
-                        <button className="product-btn">{item.name}</button>
-                ))}
-                    </div>
+                    {categories.map(item => (
+                        <button 
+                        className="product-btn" 
+                        key={item._id}
+                        onClick={() => {handleClick(item._id);}}>
+                            {item.name}
+                        </button>
+                    ))}
+                </div>
                 <div className="shop-right">
+                    {state.products.length ? (
+
                     <div className="product">
                         <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
                         <h3>Iced Coffee</h3>
@@ -65,7 +102,9 @@ function ShopMenu() {
                             <button><i class="fas fa-cart-plus"></i></button>
                         </div>
                     </div>
-
+                    ) : (
+                        <h3>You haven't added any products yet!</h3>
+                    )}
                     <div className="product">
                         <img src="https://dunkinanytime.coca-cola.com/content/dam/nagbrands/us/dunkin/en/products/iced-coffee/13-7-fl-oz/dunkin_PLP_thinMints.jpg" alt=""></img>
                         <h3>Iced Coffee</h3>
