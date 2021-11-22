3075f392f86079f0e4863f398cd967daf767a184
Topash15 <Alextopash@gmail.com>
2021-11-21 21:59:22 -0500

    removed unnecessary comment

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers";
+// import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 
@@ -44,21 +44,6 @@ function ProductItem(item) {
         </button>
       </div>
     </div>
-
-    // <div className="card px-1 py-1">
-    //   <Link to={`/products/${_id}`}>
-    //     <img
-    //       alt={name}
-    //       src={`/images/${image}`}
-    //     />
-    //     <p>{name}</p>
-    //   </Link>
-    //   <div>
-    //     <div>{quantity} {pluralize("item", quantity)} in stock</div>
-    //     <span>${price}</span>
-    //   </div>
-    //   <button onClick={addToCart}>Add to cart</button>
-    // </div>
   );
 }
 
