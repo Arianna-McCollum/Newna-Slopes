1e232779f5eb6d35a6c718757a3e9ba971592158
Ian Cohen <ian_cohen90@live.com>
2021-11-23 17:02:51 -0500

    added and styled success page, routing not working

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Success from "./pages/Success";
 import Detail from "./pages/Details"
 import Footer from "./components/Footer";
 import "./css/home.css";
@@ -46,7 +47,7 @@ function App() {
             <Route path="/signup" element={<Signup />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route exact path="/products/:id" element={<Detail />}></Route>
-            {/* <Route path="/success" element={<Success />}></Route> */}
+            <Route path="/success" element={<Success />}></Route>
           </Routes>
         </StoreProvider>
         <Footer />
